c7caedd669e562b4382e10d9f688d8fefe9a6e28
Femi Ayo-Tubosun <femiaytubosun@gmail.com>
2021-05-17 17:50:46 +0100

    Replaced MySQL with SQLite

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
 from app import create_app
+import os
+
+# create database
+if not os.path.exists('./todoDB.db'):
+    import sqlite3
+    sqlite3.connect('todoDB.db')
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(debug=False)
